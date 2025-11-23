aa7250b2b05df4f124c00a028bd2a76a763a0769
Your Nameclear <BrianA>
2025-11-23 22:59:55 +0300

    initial commit

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,30 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+let temperature = 85;       
+let timeOfDay = "evening";  
+let soilMoisture = 10;      
+
+
+
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+
+
+while (soilMoisture < 40) {
+  soilMoisture += 5;
+  console.log(soilMoisture);
+}
